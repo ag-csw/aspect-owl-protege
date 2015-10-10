60234c6d8c57163dbbd2a0ffaa1cc0a0237f3f35
Ralph <schaef@inf.fu-berlin.de>
2015-10-10 11:17:19 +0200

    Add copyright information

--- src/main/java/de/fuberlin/csw/aspectowl/protege/views/AspectPanel.java
+++ src/main/java/de/fuberlin/csw/aspectowl/protege/views/AspectPanel.java
@@ -1,3 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Freie Universitaet Berlin, Department of
+ * Computer Science. All rights reserved.
+ *
+ * This file is part of the Corporate Smart Content Project.
+ *
+ * This work has been partially supported by the InnoProfile-Transfer
+ * Corporate Smart Content project funded by the German Federal Ministry
+ * of Education and Research (BMBF) and the BMBF Innovation Initiative
+ * for the New German Laender - Entrepreneurial Regions.
+ *
+ * <http://sce.corporate-smart-content.de/>
+ *
+ * Copyright (c) 2013-2016,
+ *
+ * Freie Universitaet Berlin
+ * Institut für Informatik
+ * Corporate Semantic Web group
+ * Koenigin-Luise-Strasse 24-26
+ * 14195 Berlin
+ * <http://www.mi.fu-berlin.de/en/inf/groups/ag-csw/>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version. This library is
+ * distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA or see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package de.fuberlin.csw.aspectowl.protege.views;
 
 import java.awt.CardLayout;
