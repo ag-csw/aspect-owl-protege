3431158809a135a1c225be02d7745310894ac38d
Ralph Schaefermeier <schaef@inf.fu-berlin.de>
2016-07-17 12:13:59 +0200

    Add caching of inferred aspects for an ontology.

--- src/main/java/de/fuberlin/csw/aspectowl/inference/InferredAspectAnnotationGenerator.java
+++ src/main/java/de/fuberlin/csw/aspectowl/inference/InferredAspectAnnotationGenerator.java
@@ -36,10 +36,21 @@
  *******************************************************************************/
 package de.fuberlin.csw.aspectowl.inference;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
+import org.protege.editor.core.util.ProtegeDirectories;
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.editor.owl.model.OWLWorkspace;
 import org.protege.editor.owl.model.inference.OWLReasonerManager;
@@ -55,12 +66,14 @@ import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.parameters.Imports;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.util.InferredOntologyGenerator;
 
 import de.fuberlin.csw.aspectowl.util.AnnotatedAxiomsDuplicateFilter;
 import de.fuberlin.csw.aspectowl.util.AspectOWLUtils;
 import de.fuberlin.csw.aspectowl.util.DefaultOWLAxiomVisitorAdapter;
+import uk.ac.manchester.cs.owl.owlapi.concurrent.ConcurrentOWLOntologyImpl;
 
 /**
  * This class handles the propagation of aspects of asserted axioms to inferred
@@ -86,6 +99,8 @@ public class InferredAspectAnnotationGenerator extends DefaultOWLAxiomVisitorAda
 	private OWLOntologyManager tempOM;
 	private OWLOntology inferredAspectsOnto;
 	
+	private File cacheDir;
+	
 	private static InferredAspectAnnotationGenerator instance;
 
 	/**
@@ -106,6 +121,8 @@ public class InferredAspectAnnotationGenerator extends DefaultOWLAxiomVisitorAda
 	 */
 	public InferredAspectAnnotationGenerator(OWLWorkspace workspace) {
 		this.workspace = workspace;
+		cacheDir = new File(ProtegeDirectories.getDataDirectory(), "aspect-cache");
+		cacheDir.mkdirs();
 	}
 	
 	/**
@@ -123,6 +140,25 @@ public class InferredAspectAnnotationGenerator extends DefaultOWLAxiomVisitorAda
 		OWLReasonerManager reasonerManager = modelManager.getOWLReasonerManager();
 		OWLReasoner reasoner = reasonerManager.getCurrentReasoner();
 		OWLOntology rootOnto = reasoner.getRootOntology();
+		
+        String hash = null;
+		try {
+			hash = getHash(rootOnto);
+		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		OWLOntology cachedResult = null;
+		try {
+			cachedResult = getFromCache(hash);
+		} catch (NoSuchAlgorithmException | ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		if (cachedResult != null) {
+			return cachedResult;
+		}
+		
 		
         aspectProperties = AspectOWLUtils.getAllAspectAnnotationProperties(rootOnto);
 		
@@ -170,6 +206,12 @@ public class InferredAspectAnnotationGenerator extends DefaultOWLAxiomVisitorAda
 		tempOM = null;
 		aspectProperties = null;
 		explanationGenerator = null;
+		
+		try {
+			storeInCache(hash, inferredAspectsOnto);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return inferredAspectsOnto;
 
@@ -236,5 +278,55 @@ public class InferredAspectAnnotationGenerator extends DefaultOWLAxiomVisitorAda
 			// unsupported axiom type. So it's quite normal to end up here.
 			log.debug(e.getMessage() + ": " + axiom);
 		}
+	}
+	
+	private OWLOntology getFromCache(String hash) throws NoSuchAlgorithmException, IOException, ClassNotFoundException {
+        
+        
+        File[] dir = cacheDir.listFiles();
+        for (File file : dir) {
+			String name = file.getName();
+			if (name.startsWith(hash)) {
+				ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+				ConcurrentOWLOntologyImpl result = (ConcurrentOWLOntologyImpl)(in.readObject());
+				in.close();
+				return result;
+			}
+		}
+        
+        return null;
+	}
+	
+	/**
+	 * @param hash
+	 * @param inferredAspectsOnto
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	private void storeInCache(String hash, OWLOntology inferredAspectsOnto) throws FileNotFoundException, IOException {
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(cacheDir, hash)));
+		out.writeObject(inferredAspectsOnto);
+		out.close();
+	}
+
+
+	
+	private String getHash(OWLOntology rootOnto) throws NoSuchAlgorithmException {
+        TreeSet<OWLAxiom> axioms = new TreeSet<OWLAxiom>();
+		for (OWLAxiom axiom : rootOnto.getAxioms(Imports.INCLUDED)) {
+			axioms.add(axiom);
+		}
+        
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        md.update(axioms.toString().getBytes());
+        
+        byte[] mdbytes = md.digest();
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mdbytes.length; i++) {
+            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        
+        return sb.toString();
 	}
 }
